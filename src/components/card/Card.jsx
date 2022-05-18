d0bd2fb50ecb046f14b0eee3414e075a1f0ef89c
emanuelgimenez2 <emanuelgimenez2@gmail.com.com>
2022-05-17 22:02:50 -0300

    estilos

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Card.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Card.css";
 
-export default function Card ({min, max, name, img, onClose, id, country}) {
-    return (
-      <div className="card">
-        <div className="card-body">
+export default function Card({ min, max, name, img, onClose, id, country }) {
+  return (
+    <div className="card">
+      <div className="card-body">
         <Link to={`/ciudad/${id}`}>
-          <h5 className="card-title">{name}, {country}</h5>
+          <h5 className="card-title">
+            {name}, {country}
+          </h5>
         </Link>
-          <div className="row divcontainer">
-            <div className="col-sm-4 col-md-4 col-lg-4">
-              <p className="min">Min ↓</p>
-              <p>{min}°C</p>
-            </div>
-            <div className="col-sm-4 col-md-4 col-lg-4">
-              <p className="max">Max ↑</p>
-              <p>{max}°C</p>
-            </div>
-            <div >
-              <img className="iconoClima" src={"http://openweathermap.org/img/wn/"+img+"@2x.png"} width="80" height="80" alt="" />
-            </div>
-        <div id="closeIcon" className="row">
-           
-            <button  onClick={onClose} type="button" class="btn btn-danger">Cerrar</button>
-        </div>
+        <div className="row divcontainer">
+          <div className="col-sm-4 col-md-4 col-lg-4">
+            <p className="min">Min ↓</p>
+            <p>{min}°C</p>
+          </div>
+          <div className="col-sm-4 col-md-4 col-lg-4">
+            <p className="max">Max ↑</p>
+            <p>{max}°C</p>
+          </div>
+          <div>
+            <img
+              className="iconoClima"
+              src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
+              width="80"
+              height="80"
+              alt=""
+            />
+          </div>
+          <div id="closeIcon" className="container-button">
+            <button onClick={onClose} type="button" className="btn btn-danger">
+              Cerrar
+            </button>
           </div>
         </div>
       </div>
-    );
-};
+    </div>
+  );
+}
--- src/components/city/City.jsx
+++ src/components/city/City.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import styles from '../card/Card.module.css';
-import NotFound from '../notfound/NotFound.jsx';
-import  "./City.css";
+import React from "react";
+import styles from "../card/Card.module.css";
+import NotFound from "../notfound/NotFound.jsx";
+import "./City.css";
 
+export default function Ciudad({ city }) {
+  if (city) {
+    return (
+      <div className="city">
+        <div className="ciudad">
+          <div className={styles.card}>
+            <h2 className="titulo">{city.name}</h2>
+            <div className="info">
+              <div>Temperatura: {city.temp} ºC</div>
+              <div>Clima: {city.weather}</div>
+              <div>Viento: {city.wind} km/h</div>
+              <div>Cantidad de nubes: {city.clouds}</div>
+              <div>Latitud: {city.latitud}º</div>
+              <div>Longitud: {city.longitud}º</div>
+              <div className="container-button">
+              {/* <input type="button" value="Página anterior" onClick="http://localhost:3000/"/> */}
 
-export default function Ciudad({city}) {
-    if(city) {
-        return (
-            <div className="city">
-            <div className="ciudad">
-                
+              <button type="button" value="http://localhost:3000/" class="btn btn-primary">
+                Volver
+              </button>
 
-
-                    <div className={styles.card}>
-                        <h2 className='titulo'>{city.name}</h2>
-                        <div className="info">
-                            <div>Temperatura: {city.temp} ºC</div>
-                            <div>Clima: {city.weather}</div>
-                            <div>Viento: {city.wind} km/h</div>
-                            <div>Cantidad de nubes: {city.clouds}</div>
-                            <div>Latitud: {city.latitud}º</div>
-                            <div>Longitud: {city.longitud}º</div>
-                            <button type="button" class="btn btn-primary">Volver</button>
-                        </div>
-                        {/* <div className='img'>
+              </div>
+            </div>
+            {/* <div className='img'>
                             
                         </div> */}
-                </div>
-            </div>
+          </div>
         </div>
-        )
-    }else {
-        return (
-            <div>
-                <NotFound />
-            </div>
-        );
-    }
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <NotFound />
+      </div>
+    );
+  }
 }
